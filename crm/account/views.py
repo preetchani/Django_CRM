92c744e587d0200415c3a13241db6f1660ce12cb
preetchani <preetchani23498@gmail.com>
2021-04-16 19:54:49 +0530

    CRUD operation

--- crm/account/views.py
+++ crm/account/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Order,Customer,Product
+from .forms import OrderForm
 
 # Create your views here.
 def home(request):	
@@ -31,3 +32,38 @@ def customer(request,pk_test):
 
 	context = {'customer':customer, 'orders':orders, 'order_count':order_count}
 	return render(request, 'account/customer.html',context)
+
+def createOrder(request):
+	form = OrderForm()
+	if request.method == 'POST':
+		
+		form = OrderForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('/')
+
+	context = {'form':form}
+	return render(request, 'account/order_form.html', context)
+
+def updateOrder(request, pk):
+
+	order = Order.objects.get(id=pk)
+	form = OrderForm(instance=order)
+
+	if request.method == 'POST':
+		form = OrderForm(request.POST, instance=order)
+		if form.is_valid():
+			form.save()
+			return redirect('/')
+
+	context = {'form':form}
+	return render(request, 'account/order_form.html', context)
+
+def deleteOrder(request, pk):
+	order = Order.objects.get(id=pk)
+	if request.method == "POST":
+		order.delete()
+		return redirect('/')
+
+	context = {'item':order}
+	return render(request, 'account/delete.html', context)
